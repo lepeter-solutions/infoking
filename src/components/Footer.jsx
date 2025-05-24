61224027e53724126727a03faa81a48c328b05ed
Gmat <galganmate10@gmail.com>
2025-05-25 01:10:05 +0200

    changed date from 2023 to 2025

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-gray-800 text-white text-center p-4">
-      <p>&copy; 2023 Infoking. All rights reserved.</p>
+      <p>&copy; 2025 Infoking. All rights reserved.</p>
     </footer>
   );
 }
