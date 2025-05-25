e857c79e3878fe5dfbfcfe53f0cfed8ca0c508ca
Gmat <galganmate10@gmail.com>
2025-05-25 02:03:49 +0200

    infoking is now clickable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="bg-blue-600 text-white p-4">
       <div className="w-full mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Infoking</h1>
+        <Link
+          to="/"
+          className="text-2xl font-bold hover:underline hover:text-white focus:text-white active:text-white"
+          style={{ color: 'inherit' }}
+        >
+          Infoking
+        </Link>
         <nav>
           <ul className="flex space-x-4">
-            <li><a href="/" className="hover:underline">Home</a></li>
-            <li><a href="/about" className="hover:underline">About</a></li>
-            <li><a href="/contact" className="hover:underline">Contact</a></li>
+            <li>
+              <Link to="/" className="hover:underline hover:text-white focus:text-white active:text-white" style={{ color: 'inherit' }}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="hover:underline hover:text-white focus:text-white active:text-white" style={{ color: 'inherit' }}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:underline hover:text-white focus:text-white active:text-white" style={{ color: 'inherit' }}>
+                Contact
+              </Link>
+            </li>
           </ul>
         </nav>
       </div>
